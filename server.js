509ae941dbba5201222e53796cb89994904f2fa1
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-04 21:49:10 -0500

    added routes to server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const apiRoutes = require('./routes/apiRoutes');
+const homeRoutes = require('./routes/homeRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -7,5 +10,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+//routes
+app.use(apiRoutes);
+app.use(homeRoutes)
 
 app.listen(PORT, () => console.log('App listening...'));
