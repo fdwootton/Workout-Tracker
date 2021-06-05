07104e7231a3be4883a043ed22e2d2c58238efa8
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-05 18:54:41 -0500

    added put, post, get routes to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,14 +3,47 @@ const router = require('express').Router();
 const workoutDB = require('../models/workout');
 
 //GET request for all workouts
-router.get('/api/workout', function(req, res) {
+router.get('/api/workouts', function(req, res) {
     workoutDB.find({})
-    .then(function(dbData){
-        res.json(dbData);
+    .then(function(data){
+        res.json(data);
     })
     .catch(function(error){
         res.json(error);
     });
+});
+
+// PUT request to add exercise
+router.put('/api/workouts/:id', function ({ body, params }, res) {
+	workoutDB.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+		.then(function(data){
+			res.json(data);
+		})
+		.catch(function(error){
+			res.json(error);
+		});
+});
+
+// POST request to create new workout
+router.post("/api/workouts", function({ body }, res) {
+	workoutDB.create(body)
+		.then(function(data) {
+			res.json(data);
+		})
+		.catch(function(error) {
+			res.json(error);
+		});
+});
+
+// GET request for workouts in range
+router.get("/api/workouts/range", (req, res) => {
+	workoutDB.find()
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
 });
 
 module.exports = router;
