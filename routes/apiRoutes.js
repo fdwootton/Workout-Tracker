fdc24c2660b8810d2bc2d17698f75ca78c9f01d2
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-05 16:51:32 -0500

    edited server.js and apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 
 router.get('/api/workout', function(req, res) {
+    console.log('api/workouts route hit')
+    res.end()
+    
+    Workout.find({})
+    .then(function(){})
+    
     //logic
 
     res.json();
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const homeRoutes = require('./routes/homeRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
