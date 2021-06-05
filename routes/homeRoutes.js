6b0040dd62fdbb72a9986839cc65498862c5e5b7
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-05 17:10:13 -0500

    added home page route to homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
+
 const path = require('path');
 
+//GET request for home page
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+//GET request for exercise page
 router.get('/exercise', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/exercise.html'))
-})
+});
 
+//GET request for stats page
 router.get('/stats', function(req, res){
     res.sendFile(path.join(__dirname, '../public/stats.html'))
-})
+});
 
 module.exports = router;
