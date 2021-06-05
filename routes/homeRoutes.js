9495dfd88381f03b434edeb87a60624119a72abd
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-04 21:55:44 -0500

    added sendFile to router.get in homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,9 +1,12 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const path = require('path');
 
 router.get('/exercise', function(req, res) {
-
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
 })
 
-router.get('/stats')
+router.get('/stats', function(req, res){
+    
+})
 
 module.exports = router;
