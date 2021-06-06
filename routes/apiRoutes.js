b3c196d7a4c6ee25ddc21f4252792abaddf9983b
Felicia Wootton <felicia.wootton@gmail.com>
2021-06-06 11:52:19 -0500

    changed req.body in apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,8 +25,9 @@ router.put('/api/workouts/:id', function ({ body, params }, res) {
 });
 
 // POST request to create new workout
-router.post("/api/workouts", function({ body }, res) {
-	workoutDB.create(body)
+router.post("/api/workouts", function( req
+    , res) {
+	workoutDB.create(req.body)
 		.then(function(data) {
 			res.json(data);
 		})
@@ -36,8 +37,8 @@ router.post("/api/workouts", function({ body }, res) {
 });
 
 // GET request for workouts in range
-router.get("/api/workouts/range", function(req, res) {
-	workoutDB.find()
+router.get("/api/workouts/range", function({query}, res) {
+	workoutDB.find({ day: { $gte: query.start, $lte: query.end } })
 		.then(function(data) {
 			res.json(data);
 		})
